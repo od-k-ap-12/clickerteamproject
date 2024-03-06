8a22fb92c1d825d1e8dbf021ef5bbf65359d9f80
od-k-ap-12 <predictormagnolia@gmail.com>
2024-03-06 12:01:51 +0200

    cursor

--- js/titlescreen.js
+++ js/titlescreen.js
@@ -16,3 +16,15 @@ document.addEventListener('keydown', function (event) {
     link.click();
   });
 
+document.addEventListener('mousemove', function (e) {
+    const dot = document.getElementById('customCursor');
+    var rect = titlescreen.getBoundingClientRect();
+    // размер окна * 0.9(= размер gameArea) / ширину или высоту 
+    let scaleX = window.innerWidth * 0.9 / rect.width;
+    let scaleY = window.innerHeight * 0.9 / rect.height;
+    dot.style.left = (e.pageX - rect.left) * scaleX + 'px';
+    dot.style.top = (e.pageY - rect.top) * scaleY + 'px';
+    console.log(dot.style.left);
+    console.log(dot.style.top);
+});
+
